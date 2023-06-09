5057770d10b2685a645ac8d236d94e3d9d452ce2
simone <simone@elementalconcept.com>
2023-06-09 13:56:14 +0200

    test

--- projects/lib/src/lib/components/dynamic-form/dynamic-form.component.ts
+++ projects/lib/src/lib/components/dynamic-form/dynamic-form.component.ts
@@ -122,6 +122,7 @@ export class DynamicFormComponent<M> implements OnInit {
         delay(0, asapScheduler)
       )
       .subscribe(this.renderForm);
+
   }
 
   onSubmit = () => this.formSubmit.emit({ formGroup: this.formGroup, value: this.formGroup.value });
