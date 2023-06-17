d776649bf5e50188526442f86a5c97fe963cbece
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2023-06-17 16:20:49 +0100

    Removed ngx-translate dependency

--- projects/lib/src/lib/services/index.ts
+++ projects/lib/src/lib/services/index.ts
@@ -1,4 +1,2 @@
 export * from './dynamic-form-factory/dynamic-form-factory.service';
 export * from './dynamic-form-factory/dynamic-form-validators';
-
-export * from './translation-filter/translation-filter.service';
